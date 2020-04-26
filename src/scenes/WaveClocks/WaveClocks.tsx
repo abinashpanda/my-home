85d2dfbe0185fc0a27bc198be504d49afbf60ade
Abinash Panda <abinash@prodios.com>
2020-04-26 19:59:10 +0530

    Update page styles

--- src/scenes/WaveClocks/WaveClocks.tsx
+++ src/scenes/WaveClocks/WaveClocks.tsx
@@ -19,40 +19,22 @@ const WaveClocks = () => {
   return (
     <div className="w-screen min-h-screen overflow-x-hidden bg-gray-100">
       <div className="max-w-6xl py-4 mx-auto">
-        <div className="flex items-center justify-between mb-4">
-          <div>
-            <div className="text-3xl font-semibold text-gray-900">
-              Wave Clocks
-            </div>
-            <div className="text-sm text-gray-700">
-              Inspired from{' '}
-              <a href="https://www.manning.com/books/generative-art">
-                <span className="font-medium text-teal-700">
-                  Generative Art: A practical guide to Processing
-                </span>
-              </a>
-            </div>
+        <div className="mb-12">
+          <div className="text-3xl font-semibold text-gray-900">
+            Wave Clocks
           </div>
-          <button
-            className="p-2 text-gray-200 bg-gray-800 rounded-md opacity-25 hover:opacity-100 focus:outline-none focus:shadow-outline"
-            onClick={handleRefresh}
-          >
-            <svg
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              className="w-5 h-5"
-            >
-              <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
-            </svg>
-          </button>
+          <div className="text-sm text-gray-700">
+            Inspired from{' '}
+            <a href="https://www.manning.com/books/generative-art">
+              <span className="font-medium text-teal-700">
+                Generative Art: A practical guide to Processing
+              </span>
+            </a>
+          </div>
         </div>
         <Masonry
-          className="flex -ml-4"
-          columnClassName="pl-4"
+          className="flex -ml-6"
+          columnClassName="pl-6"
           breakpointCols={{ default: 4 }}
         >
           {frames.map((frame) => (
@@ -60,10 +42,26 @@ const WaveClocks = () => {
               key={frame.id}
               height={frame.height}
               iterationCount={frame.iterationCount}
-              className="mb-4"
+              className="mb-6"
             />
           ))}
         </Masonry>
+        <button
+          className="fixed bottom-0 right-0 p-2 mb-16 mr-16 text-teal-100 bg-teal-800 rounded-md opacity-75 hover:opacity-100 focus:outline-none focus:shadow-outline"
+          onClick={handleRefresh}
+        >
+          <svg
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            className="w-5 h-5"
+          >
+            <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
+          </svg>
+        </button>
       </div>
     </div>
   )
